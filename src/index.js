c5689b3f881098c9c210bfee0040d767915dd00a
Alan-404 <81248081+Alan-404@users.noreply.github.com>
2021-10-29 21:32:56 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 587;
 require('dotenv').config();
 const cors = require('cors');
 const DB = require('./database')
